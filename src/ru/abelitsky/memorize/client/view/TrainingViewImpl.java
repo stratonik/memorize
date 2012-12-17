42283b22b7c41f2f0230223b42ab976abffd31f2
Alexandr Belitsky <stratonik@gmail.com>
2012-12-17 15:19:15 +0400

    Добавлен глобальный обрабочик нажатия Enter в TrainingView

--- src/ru/abelitsky/memorize/client/view/TrainingViewImpl.java
+++ src/ru/abelitsky/memorize/client/view/TrainingViewImpl.java
@@ -6,12 +6,17 @@ import ru.abelitsky.memorize.shared.dto.TrainingTest;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.web.bindery.event.shared.HandlerRegistration;
 
 public class TrainingViewImpl extends Composite implements TrainingView {
 
@@ -31,18 +36,33 @@ public class TrainingViewImpl extends Composite implements TrainingView {
 	@UiField
 	Button next;
 
+	private HandlerRegistration enterDownHandler;
+
 	public TrainingViewImpl() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
 	@UiHandler("stop")
 	void onClickStop(ClickEvent event) {
+		if (enterDownHandler != null) {
+			enterDownHandler.removeHandler();
+			enterDownHandler = null;
+		}
 		presenter.goTo(presenter.getBackPlace());
 	}
 
 	@Override
 	public void prepareView() {
 		next.setEnabled(false);
+
+		enterDownHandler = RootPanel.get().addDomHandler(new KeyDownHandler() {
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					next.click();
+				}
+			}
+		}, KeyDownEvent.getType());
 	}
 
 	@Override
---
+++ src/ru/abelitsky/memorize/client/widget/training/TrainingWidget.java
@@ -0,0 +1,13 @@
+package ru.abelitsky.memorize.client.widget.training;
+
+import ru.abelitsky.memorize.shared.dto.TrainingTest;
+
+import com.google.gwt.user.client.ui.IsWidget;
+
+public interface TrainingWidget extends IsWidget {
+
+	boolean checkAnswer();
+
+	void setData(TrainingTest test);
+
+}
